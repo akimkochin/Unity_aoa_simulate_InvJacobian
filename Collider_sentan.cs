d996088cc75de88ac4459530020d733c4e7dc577
kojima <is0165hp@ed.ritsumei.ac.jp>
2019-03-12 18:50:13 +0900

    当たり判定で、キューブの色を変更

--- Collider_sentan.cs
+++ Collider_sentan.cs
@@ -3,17 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Collider_sentan : MonoBehaviour {
+    Material mat;
+    Key_Input Key;
+    GameObject passive;
+
 
 	// Use this for initialization
 	void Start () {
-		
+        passive = GameObject.Find("PassiveBase");
+        Key = passive.GetComponent<Key_Input>();
+        mat = this.gameObject.GetComponent<Renderer>().material;
 	}
 	
 	// Update is called once per frame
 	void OnTriggerEnter (Collider t)
     {
-        Time.timeScale = 0;
-        print("collider");
+        print("flag" + Key_Input.collider_flag);
+        if (Key_Input.collider_flag == 0)
+        {
+            mat.color = Color.blue;
+            Key_Input.collider_flag += 1;
+        }else if(Key_Input.collider_flag == 1)
+        {
+            Time.timeScale = 0;
+            print("collider");
+        }
+
+        
 	}
     void Update()
     {
